ab7dc897b91c6c9eb805789012eba7bf33f0b8b9
shurxet <s.shurupov.88@mail.ru>
2024-11-19 02:55:17 +0300

    update alembic/env.py

--- fast_api/alembic/env.py
+++ fast_api/alembic/env.py
@@ -1,147 +1,4 @@
 # alembic/env.py
-
-
-# import os
-# import sys
-#
-# from dotenv import load_dotenv
-# from logging.config import fileConfig
-# from sqlalchemy import engine_from_config
-# from sqlalchemy import pool
-# from alembic import context
-# from app.models import Base  # Убедитесь, что импорт правильный
-# from app.core.config import settings  # Импортируем настройки из config.py
-#
-#
-# # Загружаем переменные окружения из файла .env
-# load_dotenv()
-#
-# # DATABASE_URL = "postgresql://user:password@localhost/dbname"
-#
-# # # Получаем конфигурацию Alembic
-# # config = context.config
-# #
-# # # Загружаем конфигурацию логирования
-# # if config.config_file_name is not None:
-# #     fileConfig(config.config_file_name)
-#
-# # Устанавливаем URL базы данных из переменной окружения
-# # url = os.getenv('DATABASE_URL')
-# # if url is None:
-# #     raise ValueError("DATABASE_URL environment variable not set.")
-# # config.set_main_option('sqlalchemy.url', url)
-#
-# # # Устанавливаем URL базы данных из переменных окружения, получаем из settings
-# # url = settings.DATABASE_URL  # Используем строку подключения из settings
-# # if url is None:
-# #     raise ValueError("DATABASE_URL environment variable not set.")
-# # config.set_main_option('sqlalchemy.url', url)
-#
-#
-#
-# # Проверяем, если это тестовая среда
-# if 'pytest' in sys.modules:
-#     # Пропускаем конфигурацию Alembic при тестировании
-#     config = None
-# else:
-#     # Настройка конфигурации для работы в реальной среде
-#     config = context.config
-#
-#     # Загружаем конфигурацию логирования
-#     if config.config_file_name is not None:
-#         fileConfig(config.config_file_name)
-#
-#     url = settings.DATABASE_URL
-#     if url is None:
-#         raise ValueError("DATABASE_URL environment variable not set.")
-#     config.set_main_option('sqlalchemy.url', url)
-#
-#
-# # Устанавливаем target_metadata для автоматической генерации миграций
-# target_metadata = Base.metadata
-#
-#
-# # def run_migrations_offline() -> None:
-# #     """Запускает миграции в оффлайн-режиме."""
-# #     context.configure(
-# #         url=url,
-# #         target_metadata=target_metadata,
-# #         literal_binds=True,
-# #         dialect_opts={"paramstyle": "named"},
-# #     )
-# #
-# #     with context.begin_transaction():
-# #         context.run_migrations()
-# #
-# #
-# # def run_migrations_online() -> None:
-# #     """Запускает миграции в онлайн-режиме."""
-# #     connectable = engine_from_config(
-# #         config.get_section(config.config_ini_section, {}),
-# #         prefix="sqlalchemy.",
-# #         poolclass=pool.NullPool,
-# #     )
-# #
-# #     with connectable.connect() as connection:
-# #         context.configure(
-# #             connection=connection,
-# #             target_metadata=target_metadata
-# #         )
-# #
-# #         with context.begin_transaction():
-# #             context.run_migrations()
-#
-#
-# # if context.is_offline_mode():
-# #     run_migrations_offline()
-# # else:
-# #     run_migrations_online()
-#
-#
-#
-# # Выполнение миграций в зависимости от режима
-#
-#
-# def run_migrations_offline() -> None:
-#     """Запускает миграции в оффлайн-режиме."""
-#     context.configure(
-#         url=url,
-#         target_metadata=target_metadata,
-#         literal_binds=True,
-#         dialect_opts={"paramstyle": "named"},
-#     )
-#
-#     with context.begin_transaction():
-#         context.run_migrations()
-#
-# def run_migrations_online() -> None:
-#     """Запускает миграции в онлайн-режиме."""
-#     connectable = engine_from_config(
-#         config.get_section(config.config_ini_section, {}),
-#         prefix="sqlalchemy.",
-#         poolclass=pool.NullPool,
-#     )
-#
-#     with connectable.connect() as connection:
-#         context.configure(
-#             connection=connection,
-#             target_metadata=target_metadata
-#         )
-#
-#         with context.begin_transaction():
-#             context.run_migrations()
-#
-#
-# if config and context.is_offline_mode():
-#     run_migrations_offline()
-# else:
-#     run_migrations_online()
-
-
-
-
-
-import os
 import sys
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
@@ -170,6 +27,7 @@ else:
 # Подключение метаданных для миграций
 target_metadata = Base.metadata
 
+
 def run_migrations_offline() -> None:
     """Запускает миграции в оффлайн-режиме."""
     context.configure(
@@ -181,6 +39,7 @@ def run_migrations_offline() -> None:
 
     with context.begin_transaction():
         context.run_migrations()
+
 
 def run_migrations_online() -> None:
     """Запускает миграции в онлайн-режиме."""
@@ -199,6 +58,7 @@ def run_migrations_online() -> None:
 
             with context.begin_transaction():
                 context.run_migrations()
+
 
 # Выполнение миграций в зависимости от режима
 if config and context.is_offline_mode():
